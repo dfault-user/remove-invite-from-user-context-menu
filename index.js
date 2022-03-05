0134ce043f818d970d217cc10e305906f4084b0e
Brandan <brandandelafuente@gmail.com>
2022-03-05 16:16:20 -0500

    test modify

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const { Plugin } = require('powercord/entities');
+const { Plugin } = require('powercord/entities')
 
 module.exports = class RemoveInviteFromUserContextMenu extends Plugin {
     async startPlugin () {
+	console.log("you smell like you farted");
+	console.log("farted");
         this.loadStylesheet('style.css');
     }
 }
