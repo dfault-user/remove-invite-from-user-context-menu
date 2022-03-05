8b7659d2a94ff2c34b94b4ff8c7fb04b7d8e9cbd
Brandan <brandandelafuente@gmail.com>
2022-03-05 16:19:02 -0500

    ok now revert

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const { Plugin } = require('powercord/entities')
 
 module.exports = class RemoveInviteFromUserContextMenu extends Plugin {
     async startPlugin () {
-	console.log("you smell like you farted");
-	console.log("farted");
         this.loadStylesheet('style.css');
     }
 }
